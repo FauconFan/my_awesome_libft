400d8c87217411d201c570e824ab5c9e1dc1c0bb
fauconfan <joseph0priou@gmail.com>
2018-06-28 16:13:22 +0200

    add some test to isalnum and use build custom on each file generated by ctestcom now

--- libft/src/libft_char/ft_isalnum.c
+++ libft/src/libft_char/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 12:06:58 by jpriou            #+#    #+#             */
-/*   Updated: 2018/06/28 15:18:19 by jpriou           ###   ########.fr       */
+/*   Updated: 2018/06/28 16:02:12 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,42 @@
 /*
 ** Testcom :
 **	assert(ft_isalnum('O') == 1);
+**	assert(ft_isalnum('L') == 1);
+**	assert_short(ft_isalnum('0'), 0, NEQU);
+**	assert_short(ft_isalnum('1'), 0, NEQU);
+**	assert_short(ft_isalnum('2'), 0, NEQU);
+**	assert_short(ft_isalnum('3'), 0, NEQU);
+**	assert_short(ft_isalnum('4'), 0, NEQU);
+**	assert_short(ft_isalnum('5'), 0, NEQU);
+**	assert_short(ft_isalnum('6'), 0, NEQU);
+**	assert_short(ft_isalnum('7'), 0, NEQU);
+**	assert_short(ft_isalnum('8'), 0, NEQU);
+**	assert_short(ft_isalnum('9'), 0, NEQU);
+**
+**	for (int c = 'a'; c <= 'z'; c++) {
+**		assert_short(ft_isalnum(c), 0, NEQU);
+**	}
+**
+**	for (int c = 'A'; c <= 'Z'; c++) {
+**		assert_short(ft_isalnum(c), 0, NEQU);
+**	}
+**
+**	assert_short(ft_isalnum(':'), 0, EQU);
+**	assert_short(ft_isalnum(';'), 0, EQU);
+**	assert_short(ft_isalnum('/'), 0, EQU);
+**	assert_short(ft_isalnum('!'), 0, EQU);
+**	assert_short(ft_isalnum('!'), 0, EQU);
+**	assert_short(ft_isalnum(','), 0, EQU);
+**	assert_short(ft_isalnum('.'), 0, EQU);
+**	assert_short(ft_isalnum('?'), 0, EQU);
+**	assert_short(ft_isalnum('&'), 0, EQU);
+**	assert_short(ft_isalnum('\"'), 0, EQU);
+**	assert_short(ft_isalnum('\''), 0, EQU);
+**	assert_short(ft_isalnum('('), 0, EQU);
+**	assert_short(ft_isalnum('-'), 0, EQU);
+**	assert_short(ft_isalnum('_'), 0, EQU);
+**	assert_short(ft_isalnum('@'), 0, EQU);
+**
 */
 t_bool		ft_isalnum(int c)
 {
--- scripts/exec_ctestcom.sh
+++ scripts/exec_ctestcom.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 LIBFT_FOLDER="libft"
-LIBFT_SRC="libft/src/"
 CTESTCOM_FOLDER="ctestcom"
+BUILD_CUSTOM_FOLDER="build_custom"
+
+LIBFT_SRC="libft/src/"
 PATH_CTESTCOM="ctestcom/ctestcom"
-FLAGS_COMP_LIBFT="-L ${LIBFT_FOLDER} -l ft -I ${LIBFT_FOLDER}"
+FLAGS_COMP_LIBFT="-L rendu -l ft -I rendu"
 FLAGS_COMP_CTESTCOM="-L ctestcom/libtestcom -l ctestcom -I ctestcom/libtestcom/includes"
-OUTPUT_CTESTCOM=$(mktemp)
-OUTPUT_POST_BUILD=$(mktemp)
+TMP_FOLDER=$(mktemp -d)
+OUTPUT_CTESTCOM=${TMP_FOLDER}/ctestcom_output.c
+OUTPUT_POST_BUILD=${TMP_FOLDER}/post_build_ctestcom.c
 
 NB_FILES=0
 FILES_WITH_TESTS=0
@@ -30,28 +33,30 @@ printf "Building ctestcom... "
 make -C ${CTESTCOM_FOLDER} > /dev/null 2>&1
 printf "built\\n"
 
-printf "Building libft... "
-make -C ${LIBFT_FOLDER} > /dev/null 2>&1
+printf "Building build_custom... "
+make -C ${BUILD_CUSTOM_FOLDER} > /dev/null 2>&1
 printf "built\\n"
 
 for FILE in ${FILES}; do
     NB_FILES=$((${NB_FILES} + 1))
     printf "Handling %s\\n" "${FILE}"
-    ./${PATH_CTESTCOM} ${FILE} ${OUTPUT_CTESTCOM}.c
-    gcc ${OUTPUT_CTESTCOM}.c ${FLAGS_COMP_CTESTCOM} ${FLAGS_COMP_LIBFT} -o ${OUTPUT_POST_BUILD}
+    ./${PATH_CTESTCOM} ${FILE} ${OUTPUT_CTESTCOM}
+    make -C ${BUILD_CUSTOM_FOLDER} run PARAM=${OUTPUT_CTESTCOM}
+    make -C rendu
+    gcc ${OUTPUT_CTESTCOM} ${FLAGS_COMP_CTESTCOM} ${FLAGS_COMP_LIBFT} -o ${OUTPUT_POST_BUILD}
     RET_OUTPUT=$(${OUTPUT_POST_BUILD})
     OUTPUT_RC=$?
     echo "${RET_OUTPUT}"
     if ! [[ ${OUTPUT_RC} = 0 ]]; then
         echo "Tests not passed"
         RC=1
-        break
     fi
     if echo ${RET_OUTPUT} | grep 'enough tests' > /dev/null 2>&1; then
         FILES_WITH_NO_TESTS=$((${FILES_WITH_NO_TESTS} + 1))
     else
         FILES_WITH_TESTS=$((${FILES_WITH_TESTS} + 1))
     fi
+    rm -rf rendu
 done
 
 printf "Files in total :         %s\\n" "${NB_FILES}"
@@ -59,6 +64,6 @@ printf "Files with no tests :    %s\\n" "${FILES_WITH_NO_TESTS}"
 printf "Files with tests :       %s\\n" "${FILES_WITH_TESTS}"
 
 
-rm -rf ${OUTPUT_CTESTCOM} ${OUTPUT_CTESTCOM}.c ${OUTPUT_POST_BUILD}
+rm -rf ${TMP_FOLDER}
 
 exit ${RC}
