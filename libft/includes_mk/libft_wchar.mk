7f3a181dfe28a5b17f52dba2666078f8a20fc4de
fauconfan <joseph0priou@gmail.com>
2018-07-16 12:01:05 +0200

    add wchar...

---
+++ libft/includes_mk/libft_wchar.mk
@@ -0,0 +1,18 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fauconfan <fauconfan@student.42.fr>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/12/03 15:58:37 by fauconfan         #+#    #+#              #
+#    Updated: 2017/12/03 16:02:20 by fauconfan        ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+LIBFT_CHAR_FOLDER = $(SRC_FOLDER)libft_wchar/
+
+SRC_LIBFT_CHAR = ft_wcharlen.c \
+				ft_fill_wstring.c \
+
+SRC += $(addprefix $(LIBFT_CHAR_FOLDER), $(SRC_LIBFT_CHAR))
