ce181d175ea52c4d8c197cdcfcb23c6e204353ef
fauconfan <joseph0priou@gmail.com>
2018-08-01 14:20:49 +0200

    remove useless tests isalnum

--- libft/src/libft_char/ft_isalnum.c
+++ libft/src/libft_char/ft_isalnum.c
@@ -6,52 +6,12 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 12:06:58 by jpriou            #+#    #+#             */
-/*   Updated: 2018/06/28 16:02:12 by jpriou           ###   ########.fr       */
+/*   Updated: 2018/08/01 14:20:34 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-** Testcom :
-**	assert(ft_isalnum('O') == 1);
-**	assert(ft_isalnum('L') == 1);
-**	assert_short(ft_isalnum('0'), 0, NEQU);
-**	assert_short(ft_isalnum('1'), 0, NEQU);
-**	assert_short(ft_isalnum('2'), 0, NEQU);
-**	assert_short(ft_isalnum('3'), 0, NEQU);
-**	assert_short(ft_isalnum('4'), 0, NEQU);
-**	assert_short(ft_isalnum('5'), 0, NEQU);
-**	assert_short(ft_isalnum('6'), 0, NEQU);
-**	assert_short(ft_isalnum('7'), 0, NEQU);
-**	assert_short(ft_isalnum('8'), 0, NEQU);
-**	assert_short(ft_isalnum('9'), 0, NEQU);
-**
-**	for (int c = 'a'; c <= 'z'; c++) {
-**		assert_short(ft_isalnum(c), 0, NEQU);
-**	}
-**
-**	for (int c = 'A'; c <= 'Z'; c++) {
-**		assert_short(ft_isalnum(c), 0, NEQU);
-**	}
-**
-**	assert_short(ft_isalnum(':'), 0, EQU);
-**	assert_short(ft_isalnum(';'), 0, EQU);
-**	assert_short(ft_isalnum('/'), 0, EQU);
-**	assert_short(ft_isalnum('!'), 0, EQU);
-**	assert_short(ft_isalnum('!'), 0, EQU);
-**	assert_short(ft_isalnum(','), 0, EQU);
-**	assert_short(ft_isalnum('.'), 0, EQU);
-**	assert_short(ft_isalnum('?'), 0, EQU);
-**	assert_short(ft_isalnum('&'), 0, EQU);
-**	assert_short(ft_isalnum('\"'), 0, EQU);
-**	assert_short(ft_isalnum('\''), 0, EQU);
-**	assert_short(ft_isalnum('('), 0, EQU);
-**	assert_short(ft_isalnum('-'), 0, EQU);
-**	assert_short(ft_isalnum('_'), 0, EQU);
-**	assert_short(ft_isalnum('@'), 0, EQU);
-**
-*/
 t_bool		ft_isalnum(int c)
 {
 	return (((c >= '0' && c <= '9') ||
