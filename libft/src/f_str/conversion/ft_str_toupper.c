78d9ff160585188a13473031b163cbcf303b6bf3
fauconfan <joseph0priou@gmail.com>
2019-04-06 09:43:06 +0200

    fix warning gcc 8 cast-fonction-type

--- libft/src/f_str/conversion/ft_str_toupper.c
+++ libft/src/f_str/conversion/ft_str_toupper.c
@@ -12,7 +12,18 @@
 
 #include "libft.h"
 
+/**
+** Trick to avoid type cast warning on gcc8 and +
+** https://github.com/libsigcplusplus/libsigcplusplus/issues/1
+*/
+
 char	*ft_str_toupper(char *str)
 {
-	return (ft_strmap(str, (char (*)(char))ft_toupper));
+	union {
+		char	(*char_ptr)(char);
+		int		(*int_ptr)(int);
+	} u;
+
+	u.int_ptr = ft_toupper;
+	return (ft_strmap(str, u.char_ptr));
 }
